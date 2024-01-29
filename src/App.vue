3db14f871f6210733582cf37593d1ab59bcd730b
Yaroslav Dombrovskiy <yaroslav.dombrovskyi@nixs.com>
2024-01-29 16:45:48 +0100

    Easy start with Vue.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
-// JS
+import img from "./assets/images/img.jpg"
+import Card from "@/components/Card.vue";
+
+ const data = [
+   { name: "Bart Simpson", age: 10, class: "fourth grade", img }
+ ]
 </script>
 
 <template>
-<!--  HTML-->
   <h1>Hello Vue</h1>
+  <Card :data="data" />
 </template>
 
 <style scoped>
+ul, li {
+    list-style: none;
+  }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,49 @@
+<script setup>
+  const props = defineProps({
+    data: {
+      required: true,
+      default: []
+    }
+  });
+</script>
+
+<template>
+   <ul class="card__container">
+     <li
+         class="card__item"
+         v-for="el in props.data"
+         :key="el.id">
+       <h2 class="card__title">{{el.name}}</h2>
+       <img
+           :src="el.img"
+           alt="el.name"
+           class="card__img"
+       >
+       <h4 class="card__age">Age: {{el.age}}</h4>
+       <p class="card__description">Class: {{el.class}}</p>
+     </li>
+   </ul>
+</template>
+
+<style scoped>
+  .card__container {
+    padding: 12px 22px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
+  }
+  .card__title {
+    font-size: 26px;
+  }
+  .card__img {
+    border-radius: 4px;
+    max-width: 260px;
+  }
+  .card__age {
+    font-size: 22px;
+    font-weight: 500;
+  }
+  .card__description {
+    font-weight: 300;
+  }
+</style>
